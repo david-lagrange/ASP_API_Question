b04e10c5a99062b1fdf5c46c90ed3f5feb889c5b
david-lagrange <davidnlagrange@hotmail.com>
2023-07-10 14:34:07 -0500

    Added relevant project files for support

---
+++ InventoryController.cs
@@ -0,0 +1,42 @@
+﻿using Entities.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chief_river_nursery_backend.Presentation.Controllers
+{
+    [Route("api/inventory")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+
+        private readonly IServiceManager _service;
+
+        public InventoryController(IServiceManager service) => _service = service;
+
+        [HttpGet]
+        public IActionResult GetInventory()
+        {
+
+            var inventory = _service.InventoryService.GetAllInventory(trackChanges: false);
+
+            return Ok(inventory);
+
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetInventory(Guid id)
+        {
+            var inventory = _service.InventoryService.GetInventory(id, trackChanges: false);
+
+            if (inventory is null)
+                throw new InventoryNotFoundException(id);
+
+            return Ok(inventory);
+        }
+    }
+}
---
+++ InventoryNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public class InventoryNotFoundException : NotFoundException
+    {
+        public InventoryNotFoundException(Guid inventoryId) : base($"The inventory with id: {inventoryId} doesn't exist in the database.") { }
+    }
+}
